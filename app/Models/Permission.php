141aa0b2fdd263bd6c0f04b4b1e3cef9aa4e9962
Stephen Jackson <44205375+jjackson19862017@users.noreply.github.com>
2020-12-21 00:20:41 +0000

    Phpstorm indicated they were not in my git repo
    Signed-off-by: Stephen Jackson <44205375+jjackson19862017@users.noreply.github.com>

---
+++ app/Models/Permission.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Permission extends Model
+{
+    use HasFactory;
+    // Allows Mass assignments.
+    protected $guarded = [];
+
+    public function roles(): BelongsToMany
+    {
+        return $this->belongsToMany(Role::class);
+    }
+
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+
+
+
+
+}
