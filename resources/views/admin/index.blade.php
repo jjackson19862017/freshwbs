938ab298a45efc6175f40a4ae305d8fd4ab6ce8b
Stephen Jackson <44205375+jjackson19862017@users.noreply.github.com>
2021-01-05 22:22:01 +0000

    Updated Dashboard Graphs and Cards
    Signed-off-by: Stephen Jackson <44205375+jjackson19862017@users.noreply.github.com>

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -12,9 +12,10 @@
             <h1 class="h3 mb-4 text-gray-800">Dashboard</h1>
         @endif
         <div class="row">
+            <!-- Customer Cards -->
             <div class="col-lg-3 col-md-6 mb-3">
                 <div class="card text-white">
-                    <div class="card-header bg-success">
+                    <div class="card-header bg-primary">
                         <div class="row pl-4">
                             <div class="col-xs-3">
                                 <i class="fa fa-user-friends fa-4x"></i>
@@ -25,7 +26,7 @@
                         </div>
                     </div>
                     <a href="{{route('customers.index')}}">
-                        <div class="card-footer bg-gradient-success text-white text-center">
+                        <div class="card-footer bg-gradient-primary text-white text-center">
                             <span class="pull-left">View Customers</span>
                             <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
                             <div class="clearfix"></div>
@@ -33,20 +34,46 @@
                     </a>
                 </div>
             </div>
+            <!-- / Customer Cards -->
+            <!-- Unbooked Cards -->
             <div class="col-lg-3 col-md-6 mb-3">
                 <div class="card text-white">
-                    <div class="card-header bg-primary">
+                    <div class="card-header bg-danger">
+                        <div class="row pl-4">
+                            <div class="col-xs-3">
+                                <i class="fa fa-calendar-times fa-4x"></i>
+                            </div>
+                            <div class="col-xs-6 offset-3 text-right">
+                                <div class='display-4'>{{$unbooked}}</div>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="{{route('customers.unbooked')}}">
+                        <div class="card-footer bg-gradient-danger text-white text-center">
+                            <span class="pull-left">View Unbooked Events</span>
+                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                            <div class="clearfix"></div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <!-- / Unbooked Cards -->
+            <!-- Booked Cards -->
+
+            <div class="col-lg-3 col-md-6 mb-3">
+                <div class="card text-white">
+                    <div class="card-header bg-warning">
                         <div class="row pl-4">
                             <div class="col-xs-3">
                                 <i class="fa fa-calendar-day fa-4x"></i>
                             </div>
                             <div class="col-xs-6 offset-3 text-right">
-                                <div class='display-4'>{{count($wedevents)}}</div>
+                                <div class='display-4'>{{count($booked)}}</div>
                             </div>
                         </div>
                     </div>
                     <a href="{{route('wedevents.index')}}">
-                        <div class="card-footer bg-gradient-primary text-white text-center">
+                        <div class="card-footer bg-gradient-warning text-white text-center">
                             <span class="pull-left">View Booked Events</span>
                             <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
                             <div class="clearfix"></div>
@@ -54,48 +81,31 @@
                     </a>
                 </div>
             </div>
+            <!-- / Booked Cards -->
+            <!-- Completed Cards -->
             <div class="col-lg-3 col-md-6 mb-3">
                 <div class="card text-white">
-                    <div class="card-header bg-warning">
+                    <div class="card-header bg-success">
                         <div class="row pl-4">
                             <div class="col-xs-3">
-                                <i class="fa fa-user fa-4x"></i>
+                                <i class="fa fa-flag-checkered fa-4x"></i>
                             </div>
                             <div class="col-xs-6 offset-3 text-right">
-                                <div class='display-4'>{{count($users)}}</div>
+                                <div class='display-4'>{{$event_complete}}</div>
                             </div>
                         </div>
                     </div>
-                    <a href="{{route('users.index')}}">
-                        <div class="card-footer bg-gradient-warning text-white text-center">
-                            <span class="pull-left">View Users</span>
+                    <a href="{{route('wedevents.completed')}}">
+                        <div class="card-footer bg-gradient-success text-white text-center">
+                            <span class="pull-left">View Booked Events</span>
                             <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
                             <div class="clearfix"></div>
                         </div>
                     </a>
                 </div>
             </div>
-            <div class="col-lg-3 col-md-6 mb-3">
-                <div class="card text-white">
-                    <div class="card-header bg-info">
-                        <div class="row pl-4">
-                            <div class="col-xs-3">
-                                <i class="fa fa-people-carry fa-4x"></i>
-                            </div>
-                            <div class="col-xs-6 offset-3 text-right">
-                                <div class='display-4'>{{count($staff)}}</div>
-                            </div>
-                        </div>
-                    </div>
-                    <a href="{{route('staffs.index')}}">
-                        <div class="card-footer bg-gradient-info text-white text-center">
-                            <span class="pull-left">View Staff</span>
-                            <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
-                            <div class="clearfix"></div>
-                        </div>
-                    </a>
-                </div>
-            </div>
+            <!-- Completed Cards -->
+
         </div>
         <div class="row">
             <div class="col-sm-12 col-md-4">
@@ -157,6 +167,10 @@
                 }]
             },
             options: {
+                legend: {
+                    display: true,
+                    position: 'right',
+                }
             }
         });
         var ctx = document.getElementById('eventsChart');
@@ -179,6 +193,10 @@
                 }]
             },
             options: {
+                legend: {
+                    display: true,
+                    position: 'right',
+                }
             }
         });
         var ctx = document.getElementById('paymentChart');
@@ -201,6 +219,10 @@
                 }]
             },
             options: {
+                legend: {
+                    display: true,
+                    position: 'right',
+                }
             }
         });
     </script>
